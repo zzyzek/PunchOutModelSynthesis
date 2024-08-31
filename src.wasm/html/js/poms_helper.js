03e8b150174bcedb982580d129585cbbea83547b
zzyzek <zzyzek@thumpernet.com>
2024-08-30 23:35:39 -0400

    wip
    * gettint long options in
    * defaults should be set up

--- src.wasm/html/js/poms_helper.js
+++ src.wasm/html/js/poms_helper.js
@@ -16,60 +16,6 @@ var g_ui_info = {
   "worker": null
 };
 
-function example_run() {
-
-  // pillMortal_poms.json was preloaded/baked into the
-  // emscripten port, so we have a copy in data/ but
-  // that's not the copy referenced below
-  //
-  let argv = [
-    "foo",
-    "-C", "data/pillMortal_poms.json",
-    "-s", "48,48,1",
-    "-q", "64,64,1",
-    "-b", "1",
-    "-B", "8,8,1",
-    "-J", "10000",
-    "-w", "1.0",
-    "-E", "-1.95",
-    "-1", "pillMortal_64x64.json",
-    "-8", "pillMortal_snapshot.json",
-    "-P", "min",
-    "-O", "viz_step=50",
-    "-O", "patch-policy=pending",
-    "-S", "1337",
-    "-V", "1"
-  ];
-  main_like( Module._main, argv );
-
-  let json_txt = new TextDecoder().decode( FS.readFile("pillMortal_64x64.json") );
-
-  let out_json = JSON.parse(json_txt);
-
-
-  //EXPERIMENTAL
-  //EXPERIMENTAL
-
-  update_pixi_tilemap( out_json );
-
-  //let app = new PIXI.Application({ background: '#7f7f7f', resizeTo: window });
-  let app = new PIXI.Application({ background: '#ffffff', resizeTo: window });
-  document.body.appendChild(app.view);
-
-  app.stage.eventMode = 'static';
-  app.stage.hitArea = app.screen;
-  app.stage.addEventListener('pointermove', mouse_move);
-
-  g_tile_viewer_info.pixi.app = app;
-
-  setup_keyboard();
-
-  //EXPERIMENTAL
-  //EXPERIMENTAL
-
-  return out_json;
-}
-
 //----
 //----
 //----
@@ -79,81 +25,6 @@ function worker_update(e) {
   var data = JSON.parse(txt);
 
   update_pixi_tilemap(data);
-}
-
-//----
-//----
-//----
-
-/*
-function example_run_worker() {
-  //let app = new PIXI.Application({ background: '#7f7f7f', resizeTo: window });
-  let app = new PIXI.Application({ background: '#ffffff', resizeTo: window });
-  //document.body.appendChild(app.view);
-
-  let ele = document.getElementById("ui_gfx");
-  ele.innerHTML = '';
-  ele.appendChild(app.view);
-
-  app.stage.eventMode = 'static';
-  app.stage.hitArea = app.screen;
-  app.stage.addEventListener('pointermove', mouse_move);
-
-  g_tile_viewer_info.pixi.app = app;
-
-  setup_keyboard();
-
-
-  g_ui_info.worker = new Worker("js/poms_worker.js");
-  g_ui_info.worker.onmessage = worker_update;
-  g_ui_info.worker.postMessage("start");
-}
-
-function start_poms_worker() {
-
-  let msg = {
-    "type":"run",
-    "argv": []
-  };
-
-  let pm_argv = [
-    "-C", "data/pillMortal_poms.json",
-    "-s", "48,48,1",
-    "-q", "64,64,1",
-    "-b", "1",
-    "-B", "8,8,1",
-    "-J", "10000",
-    "-w", "1.0",
-    "-E", "-1.95",
-    "-1", "pillMortal_tiled.json",
-    "-8", "pillMortal_snapshot.json",
-    "-P", "min",
-    "-O", "viz_step=50",
-    "-O", "patch-policy=pending",
-    "-S", "1337",
-    "-V", "1"
-  ];
-
-  msg.argv = pm_argv;
-
-  g_ui_info.worker.postMessage(msg);
-}
-*/
-
-function ui_getSelect(_id) {
-  let ele = document.getElementById(_id);
-  let idx = ele.selectedIndex;
-  return ele.options[idx].value;
-}
-
-function ui_getNum(_id) {
-  let ele = document.getElementById(_id);
-  return ele.value;
-}
-
-function ui_getStr(_id) {
-  let ele = document.getElementById(_id);
-  return ele.value;
 }
 
 
@@ -171,8 +42,19 @@ function start_run() {
   let _argv = [];
   let _default_argmap = g_poms_default[tileset_name];
   for (let opt_key in _default_argmap) {
-    _argv.push(opt_key);
-    _argv.push(_default_argmap[opt_key]);
+
+    if (opt_key.length == 2) {
+      _argv.push(opt_key);
+      _argv.push(_default_argmap[opt_key]);
+    }
+    else if (opt_key.length > 2) {
+      let key_parts = opt_key.split(" ");
+      let param = key_parts[0];
+      let val = key_parts[1] + "=" + _default_argmap[opt_key];
+
+      _argv.push(param);
+      _argv.push(val);
+    }
   }
 
   g_ui_info.snapshot_fn = g_poms_default[tileset_name]["-8"];
@@ -191,6 +73,89 @@ function start_run() {
 // | |_| || | 
 //  \___/|___|
 //            
+function ui_getSelect(_id) {
+  let ele = document.getElementById(_id);
+  let idx = ele.selectedIndex;
+  return ele.options[idx].value;
+}
+
+function ui_setSelect(_id, v) {
+  let ele = document.getElementById(_id);
+
+  for (let ii=0; ii<ele.options.length; ii++) {
+    if (ele.options[ii].value == v) {
+      ele.selectedIndex = ii;
+    }
+  }
+
+}
+
+function ui_getNum(_id) {
+  let ele = document.getElementById(_id);
+  return ele.value;
+}
+
+function ui_setNum(_id, v) {
+  let ele = document.getElementById(_id);
+  ele.value = v;
+}
+
+function ui_getStr(_id) {
+  let ele = document.getElementById(_id);
+  return ele.value;
+}
+
+function ui_setStr(_id, v) {
+  let ele = document.getElementById(_id);
+  ele.value = v;
+}
+
+function ui_populate_defaults(tileset_name) {
+  let _default = g_poms_default[tileset_name];
+
+  console.log("??", tileset_name, _default);
+
+  let blocksize = _default["-s"].split(",");
+  ui_setNum("ui_blocksize_x", blocksize[0]);
+  ui_setNum("ui_blocksize_y", blocksize[1]);
+
+  let gridsize = _default["-q"].split(",");
+  ui_setNum("ui_gridsize_x", gridsize[0]);
+  ui_setNum("ui_gridsize_y", gridsize[1]);
+
+  let softensize = _default["-B"].split(":");
+  let soften_start_a = softensize[0].split(",");
+  let soften_end_a = softensize[0].split(",");
+  if (softensize.length > 1) { soften_end_a = softensize[1].split(","); }
+  ui_setNum("ui_softensize_x_start", soften_start_a[0]);
+  ui_setNum("ui_softensize_y_start", (soften_start_a.length == 1) ? soften_start_a[0] : soften_start_a[1] );
+  ui_setNum("ui_softensize_x_end", soften_end_a[0]);
+  ui_setNum("ui_softensize_y_end", (soften_end_a.length == 1) ? soften_end_a[0] : soften_end_a[1] );
+
+  let maxiter = _default["-J"];
+  ui_setNum("ui_maxiter", maxiter);
+
+  let rand_w = _default["-w"];
+  ui_setNum("ui_rand_w", rand_w);
+
+  let rand_E = _default["-E"];
+  ui_setNum("ui_rand_E", rand_E);
+
+  let seed = _default["-S"];
+  ui_setNum("ui_seed", seed);
+
+  let cell_policy = _default["-P"];
+  ui_setSelect("ui_cell_choice_policy", cell_policy);
+
+  // whoops
+  //
+  let viz_step = _default["-O viz_step"];
+  ui_setNum("ui_vizstep", viz_step);
+
+  let block_choice_policy = _default["-O patch-policy"];
+  ui_setSelect("ui_block_choice_policy", block_choice_policy);
+
+}
 
 function setup_ui_callbacks() {
 
@@ -198,6 +163,15 @@ function setup_ui_callbacks() {
     function(e) {
       start_run();
       //example_run_worker();
+    });
+
+  document.getElementById("ui_tileset").addEventListener("change",
+    function(e) {
+      let v = ui_getSelect("ui_tileset");
+
+      console.log("??", v);
+
+      ui_populate_defaults(v);
     });
 
   document.getElementById("ui_option").addEventListener("click",
