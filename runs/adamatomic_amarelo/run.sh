e0dd53c17b50d918b533bf6f6cf9d6144f282fbb
zzyzek <zzyzek@thumpernet.com>
2024-09-01 00:02:05 -0400

    cleaning up
    * cleaned up amarelo
    * amarelo still needs work to find good params
    * future seeding commented out param (xnyp, not implemented in poms)

--- runs/adamatomic_amarelo/run.sh
+++ runs/adamatomic_amarelo/run.sh
@@ -3,19 +3,19 @@
 ## image
 ../../bin/poms \
   -C ./data/amarelo_poms.json \
-  -b 1 -B 24:48 \
-  -w 2 -E -1.25 \
+  -s 48,48,1 \
+  -q 128,128,1 \
+  -b 1 \
+  -B 24:32  \
+  -J 10000 \
+  -w 1.5  \
+  -E -1.25 \
+  -P min \
+  -O 'viz_step=50' \
+  -O 'patch-policy=pending' \
   -S 1337 \
-  -V 2 \
-  -1 ./amarelo_128x128.json
+  -V 1 \
+  -1 ./data/amarelo_128x128.json \
+  -8 ./data/amarelo_snapshot.json
 
-exit
 
-###
-../../bin/poms \
-  -C ./data/amarelo_poms.json \
-  -b 1 -B 32 \
-  -w 1 -E -1.5 \
-  -S 1337 \
-  -V 2 \
-  -1 ./amarelo_128x128.json
